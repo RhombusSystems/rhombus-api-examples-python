60f0df641df8125242148ae6f50a20c572d6a742
jayadas9 <59684977+jayadas9@users.noreply.github.com>
2021-07-13 14:56:09 -0700

    Environment and Bandwidth Anomaly detection finished
    Finished

---
+++ Anomaly.py
@@ -0,0 +1,254 @@
+"""
+    Calls to find Anomalies for Environement and Bandwidth Anomalies
+
+"""
+import os
+import requests
+import pandas as pd
+import datetime
+from time import time
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import IsolationForest
+from docx import Document
+
+
+def calc_percent_NAs(df):
+    '''
+    Calculates % of NAs in a all columns.
+    Returns table of columns with % NA.
+    '''
+    nans = pd.DataFrame(df.isnull().sum().sort_values(ascending=False)/len(df), columns=['percent']) 
+    idx = nans['percent'] > 0
+    return nans[idx]
+
+def convert_milli_to_date(seconds):
+    '''
+    Converts Milliseconds into Date Format.
+    Returns Date format.
+    '''
+    return datetime.datetime.fromtimestamp(seconds/1000).strftime('%Y-%m-%dT%H:%M:%SZ')
+
+def clean_date(column, date_column):
+    '''
+    Converts date formatting to datetime format; easier to parse date.
+    Returns cleaned datetime. 
+    '''
+    if (column == "Temperature") or (column == "Humidity"): # Since Date formatting is different among evnironment vs. bandwidth
+        clean_a = [datetime.datetime.strptime(elem, '%Y-%m-%dT%H:%M:%S') for elem in date_column] # clean up datetime format
+    else: 
+        clean_a = [datetime.datetime.strptime(elem, '%Y-%m-%dT%H:%M:%S %Z') for elem in date_column] # clean up datetime format
+    return clean_a
+
+def get_time():
+    ''' 
+    Finds current time and 30 days prior in milliseconds. Also converts into date format.
+    Returns current millisecond, 30 days prior millisecond, current timestamp, and 30 dys prior stamp
+    '''
+    current_milli =  int(time() * 1000)
+    thirty_days_seconds = 2592000000
+    thirty_days_ago = current_milli - thirty_days_seconds
+    current_milli_date = convert_milli_to_date(current_milli)
+    thirty_days_ago_date = convert_milli_to_date(thirty_days_ago) 
+    return current_milli, thirty_days_ago, current_milli_date, thirty_days_ago_date
+
+def get_data(url, payload,headers,data_type):
+    '''
+    Grabs data via API request, creates and writes data to csv file
+    Returns filename.
+    '''
+    response = requests.request("POST", url, json=payload, headers=headers) # Response from API data request
+
+    current_milli, thirty_days_ago, current_milli_date, thirty_days_ago_date = get_time()
+
+    f_name = f'{data_type}-{thirty_days_ago_date}-to-{current_milli_date}.csv' # Filename
+    f = open(f_name, "w") # Creates csv file
+    f.write(response.text) # Writes data to csv file
+    f.close()
+    return f_name
+
+def wanted_anomaly_footage(perc_anomaly, column1, column2,c1_name,c2_name):
+    '''
+    Calculates amount of anomalies user wants and grabs X number of biggest anomalies. 
+    Returns wanted anomaly data and dates.
+    '''
+    # Finds amount of anomaly wanted based on % specified by user
+    amount_wanted_1 = round(len(column1)*(perc_anomaly/100) / 2)
+    amount_wanted_2 = round(len(column2)*(perc_anomaly/100) / 2)
+    
+    # Sorts anomaly data columns
+    c1_sorted = column1.sort_values(by=[c1_name])
+    c2_sorted = column2.sort_values(by=[c2_name])
+    
+    # Concats most Anomalous data 
+    column1_anomalies = pd.concat([c1_sorted.head(amount_wanted_1), c1_sorted.tail(amount_wanted_1)])
+    column2_anomalies = pd.concat([c2_sorted.head(amount_wanted_2), c2_sorted.tail(amount_wanted_2)])
+    
+    # Cleans dates of most Anomalous data
+    c1_date = clean_date(c1_name, column1_anomalies["Date"])
+    c2_date = clean_date(c2_name, column2_anomalies["Date"])
+
+    return column1_anomalies,c1_date, column2_anomalies, c2_date
+
+def footage_call(column_footage_dates,api_key, device_id, duration,column):
+    '''
+    Call to grab footage based on datetime given.
+    Returns start_time; grab_footage() downloads footage to current directory.
+    '''
+    outlier_num = 1
+    start_time = []
+    directory = os.getcwd() # currrent directory
+
+    # Loops through multiple footage dates wanted calling grab_footage()
+    for date in column_footage_dates:
+        clean_time = round(date.timestamp())
+        start_time.append(clean_time)
+        grab_footage(api_key, device_id, duration, clean_time,outlier_num,column,directory)
+        outlier_num += 1
+    return start_time
+    
+
+def grab_footage(api_key, device_id, duration,start_time,outlier_num,column,directory):
+    '''
+    Runs copy_footage_to_local_storage.py in sub-terminal and downloads footage.
+    Returns None.
+    '''
+    # Creates path for copy_footage_to_local_storage.py to output the footage to.
+    output_path = directory + f'/output{column}{outlier_num}.mp4'
+
+    # Running copy_footage_to_local_storage.pu
+    os.system(f'python3 copy_footage_to_local_storage.py --api_key {api_key} --device_id {device_id} --output {output_path} --start_time {start_time} --duration {duration}')
+    
+
+def create_report(graph_fname1, graph_fname2,data_type,column1_a,column2_a):
+    '''
+    Creates report of anomalies. 
+    Report contains graphs, list of user specified anomaly values, and path to footage.
+    Returns None.
+    '''
+    # Get path to video footage.
+    video_path = os.getcwd()
+
+    # Creates document and heading
+    document = Document()
+    document.add_heading(text=(f'{data_type} Anomaly Report'))
+    
+    # Adds Graphs
+    document.add_paragraph('Graph of outliers')
+    document.add_picture(video_path+'/'+graph_fname1)
+    document.add_picture(video_path+'/'+graph_fname2)
+    
+    # Adds list of anomalies
+    document.add_section()
+    with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
+        document.add_paragraph(f'List of anomaly datetimes and values:\n{column1_a}\n{column2_a}')
+    
+    # Adds footage path
+    document.add_section()
+    document.add_paragraph(f"Footage of anomaly found at: {video_path}")
+    document.save(f'{data_type}AnomalyReport.docx')
+
+def standardize_data(data):
+    '''
+    Standardize data.
+    Returns DataFrame of scaled data.
+    '''
+    np_scaled = StandardScaler().fit_transform(data)
+    return pd.DataFrame(np_scaled)  
+
+def train_forest(df,data,outliers_fraction):
+    '''
+    Train isolation forest.
+    Returns Added column of Anomalies.
+    '''
+    model = IsolationForest(contamination=outliers_fraction)
+    model.fit(data) 
+    df['anomaly2'] = pd.Series(model.predict(data)) #finds how far point is from standardized mean
+    
+    return df['anomaly2']
+
+def clean_anomaly(df,column):
+    '''
+    Finding and cleaning anomoly data.
+    Returns cleaned data frame of anomolies.
+    '''
+    a = df.loc[df["anomaly2"] == -1, ['Date', column]] #anomaly
+    print(f'Found {len(a)} anomalies for {column}.')
+    clean_a = clean_date(column,a["Date"])
+
+    return a, clean_a
+
+def visualize(df, clean_dates,clean_a, a, column):
+    '''
+    Visualizations. Displays graph of given data and anomalies. 
+    Return: None
+    '''
+    fig, ax = plt.subplots(figsize=(6,4)) # Creates plot size
+    ax.plot(clean_dates, df[column], color='blue', label = 'Normal') # plots x and y 
+    ax.scatter(clean_a,a[column], color='red', label = 'Anomaly') # plots anomalies
+
+    # Format for x-axis dates
+    ax.xaxis.set_major_formatter(mdates.DateFormatter('%m/%d')) 
+    _=plt.xticks(rotation=45)    
+
+    # Labeling
+    plt.xlabel('Date time')
+    plt.ylabel(column)
+    plt.title(f'Time Series of {column} by date time of search')
+
+    # Display legend and plot
+    plt.legend()
+    plt.savefig(f"{column}_graph.jpg")
+    #plt.show()
+    return (f"{column}_graph.jpg")
+
+def isolation_forest_test(df,data,clean_dates,column1,column2):
+    '''
+    Outlier test. Standardizes data, Finds Anomalies, Cleans Data and Plots graphs.
+    Returns: Anomalies and Anomaly dates of columns 1 and 2.
+    '''
+    outliers_fraction = 0.01 # percentage of outliers estimated;; Will need adjustment based on user preference
+
+    # Standardize Data
+    std_data = standardize_data(data) 
+
+    # Find Anomalies
+    anomaly_2 = train_forest(df,std_data,outliers_fraction)
+
+    # Cleaning anomaly data
+    column1_a, column1_clean_a = clean_anomaly(df, column1)
+    column2_a, column2_clean_a = clean_anomaly(df, column2) 
+    #print(column1_clean_a)
+    
+    # Plot Graph
+    column1_fname = visualize(df, clean_dates, column1_clean_a, column1_a,column1)
+    column2_fname = visualize(df, clean_dates, column2_clean_a, column2_a,column2)  
+
+    return column1_a, column1_clean_a, column1_fname,column2_a, column2_clean_a, column2_fname
+
+def seek_points(time_ms, cameraUuid, api_key):
+    '''
+    Creates seek point for given time and camera. Seen in console.
+    Returns None: 
+    '''
+    url = "https://api2.rhombussystems.com/api/camera/createFootageSeekpoints"
+
+    payload = {
+        "footageSeekPoint": {
+            "a": "CUSTOM",
+            "ts": time_ms,
+            "cdn": "Anomaly"
+        },
+        "cameraUuid": cameraUuid
+    }
+    headers = {
+        "Accept": "application/json",
+        "x-auth-scheme": "api-token",
+        "Content-Type": "application/json",
+        "x-auth-apikey": api_key
+    }
+
+    response = requests.request("POST", url, json=payload, headers=headers)
+
+    #print(response.text)
---
+++ BandwidthAnomaly.py
@@ -0,0 +1,114 @@
+'''
+    Anomoly Algorithm for Bandwidth
+
+Algorithm that takes in the user's api key and device id, 
+and finds anomolies for Uploads and Download Bandwidth within the past 30 day.
+
+Parameters: required -a API_KEY 
+            required -d DEVICE_ID
+
+Downloads .docx report file, anomaly footage, and csv of 30 days to current directory. 
+ 
+'''
+from Anomaly import *
+import pandas as pd
+import datetime
+import argparse
+
+data_type = "Bandwidth"
+
+def clean_data_2(df):
+    '''
+    Cleans Data; Converts B->MB; Drops duplicates; 
+    Returns updated DataFrame, Reformated Dates, data used for outlier test
+    '''
+    df["Upload (BYTES)"] = df["Upload (BYTES)"]/1000000 # change from bytes to MB
+    df["Download (BYTES)"] = df["Download (BYTES)"]/1000000 # change from bytes to MB
+    df = df.rename(columns={"Upload (BYTES)": "Upload (MB)","Download (BYTES)": "Download (MB)"})
+
+    df = df.drop_duplicates()
+    df['Upload (MB)'].fillna((df['Upload (MB)'].mean()), inplace=True)
+    df['Download (MB)'].fillna((df['Download (MB)'].mean()), inplace=True)
+
+    clean_dates = [datetime.datetime.strptime(elem, '%Y-%m-%dT%H:%M:%S %Z') for elem in df["Date"]]
+    return df, clean_dates, df[['Upload (MB)','Download (MB)']]
+
+def band_grab(api_key,device_id,):
+    '''
+    Grabs data from given parameters via API and writes to a CSV file
+    Returns filename.
+    '''
+    data_type = 'Bandwidth'
+    current_milli, thirty_days_ago, current_milli_date, thirty_days_ago_date = get_time()
+
+    url = "https://api2.rhombussystems.com/api/export/countReports"
+
+    payload = {
+        "uuidList": [device_id],
+        "type": "BANDWIDTH",
+        "scope": "DEVICE",
+        "interval": "QUARTERHOURLY",
+        "endDate": current_milli_date,
+        "startDate": thirty_days_ago_date
+    }
+    
+    headers = {
+        "Accept": "text/csv; charset=UTF-8",
+        "x-auth-scheme": "api-token",
+        "Content-Type": "application/json",
+        "x-auth-apikey": api_key
+    }
+
+    return get_data(url,payload, headers,data_type)
+
+
+def main():
+
+    # Parser. Gets arguements for test.
+    parser = argparse.ArgumentParser(
+        description='Creates a report and downloads footage of anomalies found in Environmental data for the past 30 days.')
+    
+    parser.add_argument('--api_key', '-a', type=str, required=True,
+    help='Rhombus API key')
+    
+    parser.add_argument('--device_id', '-d', type=str, required=True,
+    help='Device Id to pull frame from')
+
+    parser.add_argument('--perc_anomalies', '-p', type=int, required=False,
+    help='Perecent of anomalies you would like downloaded footage of; 1-100; default = 5')
+    
+    
+    args = parser.parse_args()
+
+    if args.perc_anomalies:
+        perc_anomalies = args.perc_anomalies
+    else:
+        perc_anomalies = 5
+
+    args.duration = 30
+    
+    # Grabs Data and assigns filename
+    file_name = band_grab(args.api_key,args.device_id)
+    
+    # DataFrame use for outlier test
+    df = pd.read_csv(file_name)
+    
+    # Cleans Data 
+    df, clean_dates, data = clean_data_2(df)
+
+    # Outlier Test
+    upload_a, upload_date_a, up_graph, download_a, download_date_a,down_graph = isolation_forest_test(df, data, clean_dates,"Upload (MB)",'Download (MB)')
+
+    # Get amount of anomalies for video footage via percent of anomalies user wants
+    upload_footage_anomalies,upload_footage_dates, download_footage_anomalies, download_footage_dates = wanted_anomaly_footage(perc_anomalies,upload_a,download_a, "Upload (MB)",'Download (MB)')
+    
+    # Grab footage from wanted % of anomalies
+    up_start = footage_call(upload_footage_dates, args.api_key, args.device_id, args.duration,"Upload")
+    down_start = footage_call(download_footage_dates, args.api_key, args.device_id, args.duration,"Download")
+    
+    # Create Report
+    create_report(up_graph,down_graph,data_type,upload_footage_anomalies,download_footage_anomalies)
+
+
+if __name__ == "__main__":
+    main()
