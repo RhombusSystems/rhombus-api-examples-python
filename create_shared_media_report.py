03caae27993af09b57e7f6aa5a41193cf1048862
BRinvelt <rinvelt@wisc.edu>
2021-08-03 15:53:39 -0400

    Update create_shared_media_report.py
    Added ability to unshare media using information from report

--- create_shared_media_report.py
+++ create_shared_media_report.py
@@ -1,6 +1,5 @@
 import requests
 import argparse
-from datetime import datetime
 import json
 import sys
 
@@ -15,16 +14,43 @@ class SharedMediaReport:
         self.session.headers = {"x-auth-scheme": "api-token","x-auth-apikey": self.args.apiKey}
         #Set a default base URL for api calls
         self.url = "https://api2.rhombussystems.com/api/"
-        self.curTime = int(datetime.utcnow().timestamp())
 
     #Define arguments which the user may be prompted for
     @staticmethod
     def __initArgParser():
         argParser = argparse.ArgumentParser(description='Generates a simple report on the status of shared media within the organization')
         argParser.add_argument("apiKey", help = 'Your personal api key, get this from your Rhombus Console')
+        argParser.add_argument("--removeSharedClipGroup","--rc", help = "UUID of Clip Group to remove")
+        argParser.add_argument("--removeSharedTimelapseGrop","--rt", help = "UUID of Timelapse Group to remove")
+        argParser.add_argument("--removeSharedStream","--rs", help = "UUID of Camera and UUID of stream to remove, comma separated")
         return argParser
-    
+
+    #This collection of methods simply attempt to remove shared Media as identified by CLAs
+    def removeClipGroup(self,uuid):
+        payload = {"uuid":uuid}
+        self.session.post(self.url+"event/deleteSharedClipGroupV2", json = payload)
+    def removeTimelapseGroup(self,uuid):
+        payload = {"uuid":uuid}
+        self.session.post(self.url+"video/deleteSharedTimelapseGroup", json = payload)
+    def removeStream(self,uuids):
+        if len(uuids) != 2:
+            print("Too many/few arguments included to remove stream")
+            return
+        payload = {
+            "cameraUuid":uuids[0],
+            "uuid":uuids[1]
+            }
+        self.session.post(self.url+"camera/deleteSharedLiveVideoStream", json = payload)
+
     def execute(self):
+        #Attempt to remove methods as requested by user
+        if(self.args.removeSharedClipGroup):
+            self.removeClipGroup(self.args.removeSharedClipGroup)
+        if(self.args.removeSharedTimelapseGrop):
+            self.removeTimelapseGroup(self.args.removeSharedTimelapseGrop)
+        if(self.args.removeSharedStream):
+            self.removeStream(self.args.removeSharedStream.split(","))
+
         #Collect Data and ensure expected status codes are received
         response = self.session.post(self.url+"event/getSharedClipGroupsV2")
         if response.status_code != 200:
@@ -68,9 +94,9 @@ class SharedMediaReport:
         print("--------------------------------------------------------------")
         print("Shared Streams")
         if sharedStreams:
-            print("\tCamera UUID, Password Protected, Start Time, End Time, URL")
+            print("\tCamera UUID, Stream UUID, Password Protected, Start Time, End Time, URL")
             for s in sharedStreams:
-                print("\t%s, %s, %s, %s, %s" % (s.get("cameraUuid"),s.get("passwordProtected"),int(s.get("timestampMs")/1000),s.get("expirationTime"),s.get("sharedLiveVideoStreamUrl")))
+                print("\t%s,%s, %s, %s, %s, %s" % (s.get("cameraUuid"),s.get("uuid"),s.get("passwordProtected"),int(s.get("timestampMs")/1000),s.get("expirationTime"),s.get("sharedLiveVideoStreamUrl")))
                 if not s.get("passwordProtected"):
                     print("\t\t Flag Unsecured Stream")
                 if not s.get("expirationTime"):
@@ -81,7 +107,7 @@ class SharedMediaReport:
         if sharedClipGroups:
             print("\tTitle, Description, Password Protected, Created at, Expires at")
             for cg in sharedClipGroups:
-                print("\t%s, %s, %s, %s, %s" % (cg.get("title"),cg.get("description"),cg.get("isSecured"),int(cg.get("createdAtMillis")/1000),cg.get("expirationTimeSecs")))
+                print("\t%s, %s, %s, %s, %s, %s" % (cg.get("uuid"),cg.get("title"),cg.get("description"),cg.get("isSecured"),int(cg.get("createdAtMillis")/1000),cg.get("expirationTimeSecs")))
                 if not cg.get("isSecured"):
                     print("\t\t Flag Unsecured Clip Group")
                 if not cg.get("expirationTime"):
@@ -92,7 +118,7 @@ class SharedMediaReport:
         if sharedTimelapseGroups:
             print("\tTitle, Description, Password Protected, Created at, Expires at")
             for tg in sharedTimelapseGroups:
-                print("\t%s, %s, %s, %s, %s" % (tg.get("title"),tg.get("description"),tg.get("isSecured"),int(tg.get("createdAtMillis")/1000),tg.get("expirationTimeSecs")))
+                print("\t%s, %s, %s, %s, %s, %s" % (tg.get("uuid"),tg.get("title"),tg.get("description"),tg.get("isSecured"),int(tg.get("createdAtMillis")/1000),tg.get("expirationTimeSecs")))
                 if not tg.get("isSecured"):
                     print("\t\t Flag Unsecured Timelapse Group")
                 if not tg.get("expirationTime"):
