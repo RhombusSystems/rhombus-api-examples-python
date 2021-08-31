d44c39de8384469a05c030b9e97a44b437151a1c
jayadas9 <59684977+jayadas9@users.noreply.github.com>
2021-08-31 11:02:23 -0700

    Audit and Anomaly examples (#32)

---
+++ AuditReportExamples/audit_helpers.py
@@ -0,0 +1,205 @@
+""" 
+Audit helpers.
+
+Helper functions for audit_report and user_audit_report.
+"""
+
+import os
+import json
+import csv
+import requests
+import datetime
+from time import time
+from docx import Document
+import matplotlib.pyplot as plt
+import warnings 
+warnings.filterwarnings("ignore")
+
+data_type = "Audit"
+
+def convert_milli_to_date(seconds):
+    '''
+    Converts Milliseconds into Date Format.
+    Returns Date format.
+    '''
+    return datetime.datetime.fromtimestamp(seconds/1000).strftime('%Y-%m-%dT%H:%M:%SZ')
+
+def get_time():
+    ''' 
+    Finds current time and 30 days prior in milliseconds. Also converts into date format.
+    Returns current millisecond, 30 days prior millisecond, current timestamp, and 30 dys prior stamp
+    '''
+    current_milli =  int(time() * 1000)
+    thirty_days_seconds = 2592000000
+    thirty_days_ago = current_milli - thirty_days_seconds
+    current_milli_date = convert_milli_to_date(current_milli)
+    thirty_days_ago_date = convert_milli_to_date(thirty_days_ago) 
+    return current_milli, thirty_days_ago, current_milli_date, thirty_days_ago_date
+
+
+current_milli, thirty_days_ago, current_milli_date, thirty_days_ago_date = get_time()
+
+
+def audit_grab(api_key):
+    '''
+    Grabs audit report for the last 30 days and writes it to a csv file.
+    Returns filename to csv.
+    '''
+    url = "https://api2.rhombussystems.com/api/report/getAuditFeed"
+
+    payload = {
+        "timestampMsBefore": current_milli,
+        "timestampMsAfter": thirty_days_ago
+    }
+    headers = {
+        "Accept": "application/json",
+        "x-auth-scheme": "api-token",
+        "Content-Type": "application/json",
+        "x-auth-apikey": api_key
+    }
+
+    return get_data_audit(url,payload,headers,data_type)
+
+
+def get_data_audit(url,payload,headers,data_type):
+    '''
+    Grabs data via API request, creates and writes data to csv file
+    Returns filename.
+    '''
+    response = requests.request("POST", url, json=payload, headers=headers)
+    
+    # Parsing json dictionary
+    if response.status_code != 200:
+        print("Encountered an Error")
+    response = json.loads(response.text)
+    response = response["auditEvents"]
+
+    # CSV file name
+    f_name = f'{data_type}-{thirty_days_ago_date}-to-{current_milli_date}.csv' # Filename
+
+    # Writing data to CSV file
+    with open(f_name, 'w') as csvOutput:
+        outputWriter = csv.writer(csvOutput)
+        outputWriter.writerow(list(response[0].keys()))#Write Header
+        for log in response[1:]:
+            outputWriter.writerow(list(log.values()))#Write Data
+
+    return f_name
+
+def clean_data_audit(df):
+    '''
+    Cleans Audit data. Combines city,state,country to one locations. Converts ms to date. 
+    Returns smaller and cleaner audit dataframe
+    '''
+    df["Location"] = df["sourceCity"] +','+df["sourceState"]+','+ df["sourceCountry"]
+    df['Date'] = df["timestamp"].apply(convert_milli_to_date)
+    df_cleaned = df.drop(columns=['timestamp','sourceCity','sourceCountry',"sourceState","displayText","failure",'orgUuid','targetUuid','userAgent','targetName','principalType','clientType'])
+    
+    return df_cleaned
+
+
+def find_unique_values(df,column):
+    '''
+    Returns unique values of wanted column.
+    '''
+    return df[column].unique()
+    
+
+def action_summary(df,action):
+    '''
+    Grabs dataframe summary of wanted action.
+    Returns dataframe filtered by certain action.
+    '''
+    actions = find_unique_values(df,"Action")
+    if action not in actions:
+        return "NO such action found."
+    return df.loc[df["Action"] == action]
+
+
+def user_action(df,user):
+    '''
+    Creates a dataframe of specific user's data.
+    Returns user's dataframe.
+    '''
+    user_df = df.loc[(df["principalName"] == user)]
+    if (len(user_df) == 0):
+        print("User not Found")
+    return user_df
+
+
+def column_activity_count(df,column):
+    '''
+    Returns dictionary of wanted column counts.
+    '''
+    return df[column].value_counts().to_dict()
+
+
+def user_action_count(df,user):
+    '''
+    Returns actions done by a certain user including count.
+    '''
+    user_df = user_action(df,user)
+    return column_activity_count(user_df,"action")
+
+
+def user_activity_plot(user_df,user):
+    xy_df = user_df[["Date","action"]]
+    short = xy_df["Date"].str.split(pat="T").str[0]
+    xy_df["Short Date"] = short
+    date_dic = column_activity_count(xy_df,"Short Date")
+    activity_count = dict(sorted(date_dic.items()))
+
+    plt.figure(figsize=(8, 6))
+    plt.plot(range(len(activity_count)), list(activity_count.values()))
+    plt.xticks(range(len(activity_count)), list(activity_count.keys()))
+    plt.xticks(fontsize=6)
+
+    plt.xlabel("Dates", labelpad=20, weight='bold', size=10)
+    plt.ylabel("Activity Count", labelpad=20, weight='bold', size=10)
+
+    plt.title(f"Activity count for {user} in the past 30 Days")
+    plt.savefig(f"{user}\'s_activity_graph.jpg")
+    return (f"{user}\'s_activity_graph.jpg")
+
+
+def user_report(user_df, user_actions, user_locations, user, graph_fname):
+    '''
+    Creates a report of anonymous actions, locations and dataframe. 
+    '''
+    # Get path.
+    path = os.getcwd()
+
+    # Creates document and heading
+    document = Document()
+    document.add_heading(text=(f'{user}\'s {data_type} Report'))
+    
+
+    # Adds list of Anonymous Data 
+    document.add_section()
+    document.add_paragraph(f'List of {user}\'s Actions:\n{user_actions}')
+    
+    document.add_section()
+    document.add_paragraph(f'List of Locations:\n{user_locations}')
+
+    document.add_paragraph('Graph of User Activity')
+    document.add_picture(graph_fname)
+
+
+    document.add_section()
+    document.add_paragraph(f'All of {user}\'s Data:')
+    t = document.add_table(user_df.shape[0]+1, user_df.shape[1])
+
+    # Turn dataframe into table in document
+    # add the header rows.
+    for j in range(user_df.shape[-1]):
+        t.cell(0,j).text = user_df.columns[j]
+
+    # add the rest of the data frame
+    for i in range(user_df.shape[0]):
+        for j in range(user_df.shape[-1]):
+            t.cell(i+1,j).text = str(user_df.values[i,j])
+        
+    
+    document.save(f'{user}_{data_type}Report.docx')
+
+    return None
---
+++ TimeSeriesAnomalyExamples/env_anomaly.py
@@ -0,0 +1,147 @@
+''' 
+    Anomoly Algorithm for Environment Sensors
+Algorithm that takes in the user's api key and device id, 
+and finds anomolies for Temperature and Humidity within the past 30 day.
+Parameters: required -a API_KEY 
+            required -d DEVICE_ID
+            optional -c converts to CELCIUS (default F)
+            optional -p percent of anomalies wanted (default 5 percent)
+Downloads .docx report file, anomaly footage, and csv of 30 days to current directory. 
+Command Line Input: 
+    basic case: python3 environment_anomaly.py -a {API_KEY} -d {DEVICE_ID} 
+    celcius case: python3 environment_anomaly.py -a {API_KEY} -d {DEVICE_ID} -c 
+    percent_anomaly case: python3 environment_anomaly.py -a {API_KEY} -d {DEVICE_ID} -p {% number of anomalies}
+'''
+
+from anomaly_helpers import *
+import pandas as pd
+import datetime
+import argparse
+
+data_type = 'Environment' 
+
+def C_to_F(temp):
+    '''
+    Converts C -> F.
+    Returns C temperature.
+    '''
+    return (temp*9/5)+32
+
+def clean_data(df,convert):
+    '''
+    Cleans Data for proper dataframe. Drops Tampered column and duplicates. Reformats Date.
+    Returns updated DataFrame, Reformated Dates and data used for outlier test.
+    '''
+    # Changes data from C->F
+    if convert:
+        changed_temp = []
+        for temp in df["Temperature"]:
+            changed_temp.append(C_to_F(temp))
+        df["Temperature"] = changed_temp
+    
+    # Deletes Tampered column
+    del df["Tampered"]
+    # Drops duplicate rows
+    df = df.drop_duplicates()
+    # Reformats date
+    clean_dates = [datetime.datetime.strptime(elem, '%Y-%m-%dT%H:%M:%S') for elem in get_datetime(df)]
+    return df, clean_dates, df[['Temperature','Humidity']]
+
+
+def EV_grab(api_key,device_id):
+    '''
+    Grabs data from given parameters via API and writes to CSV file.
+    Returns filename.
+    '''
+    
+    current_milli, thirty_days_ago, current_milli_date, thirty_days_ago_date = get_time()
+
+    url = "https://api2.rhombussystems.com/api/export/climateEvents"
+
+    payload = {
+        "createdBeforeMs": current_milli,
+        "createdAfterMs": thirty_days_ago,
+        "sensorUuid": device_id
+    }  
+    headers = {
+        "Accept": "application/json",
+        "x-auth-scheme": "api-token",
+        "Content-Type": "application/json",
+        "x-auth-apikey": api_key
+    }
+
+    return get_data(url,payload, headers,data_type)
+
+
+
+def main():
+    # Defaults to F
+    convert = True
+    url = "https://api2.rhombussystems.com/api/climate/getMinimalClimateStateList"
+
+    # Parser. Gets arguements for test.
+    parser = argparse.ArgumentParser(
+        description='Creates a report and downloads footage of anomalies found in Environmental data for the past 30 days.')
+    
+    parser.add_argument('--api_key', '-a', type=str, required=True,
+    help='Rhombus API key')
+    
+    parser.add_argument('--device_id', '-d', type=str, required=True,
+    help='Device Id to pull frame from')
+    
+    parser.add_argument("--celcius", '-c', help="convert to C; default F",
+                    action="store_true")
+    
+    parser.add_argument('--perc_anomalies', '-p', type=int, required=False,
+    help='Perecent of anomalies you would like downloaded footage of; 1-100; default=5')
+    
+
+    args = parser.parse_args()
+
+    if args.perc_anomalies:
+        perc_anomalies = args.perc_anomalies
+    else:
+        perc_anomalies = 5
+
+    args.duration = 30
+
+    # Checks for Celcius flag
+    if args.celcius: 
+        convert = False 
+
+    # Grabs data and assigns filename
+    file_name = EV_grab(args.api_key,args.device_id)
+
+    # DataFrame used for outlier test
+    df = pd.read_csv(file_name)
+
+    # Clean Data 
+    df, clean_dates, data = clean_data(df, convert)
+    
+    # Outlier Test
+    temp_a, temp_date_a, temp_graph, hum_a, hum_date_a, hum_graph = isolation_forest_test(df, data, clean_dates,"Temperature","Humidity")
+   
+    # Get amount of anomalies for video footage via percent of anomalies user wants
+    temp_footage_anomalies, temp_footage_dates, hum_footage_anomalies, hum_footage_dates = wanted_anomaly_footage(perc_anomalies,temp_a,hum_a,"Temperature","Humidity")
+
+    associated_cameras = find_associated_camera(args.api_key, url,"climateStates")
+
+    # Grab footage from wanted % of anomalies and creates seek points
+    for camera_id in associated_cameras:
+        temp_start = footage_call(temp_footage_dates, args.api_key, camera_id, args.duration,"Temperature")
+        hum_start = footage_call(hum_footage_dates, args.api_key, camera_id, args.duration,"Humidity")
+        
+        # Add Seek Points
+        for sec_time in temp_start:
+            start_time = sec_time * 1000
+            seek_points(start_time, camera_id, args.api_key)
+        for sec_time in hum_start:
+            start_time = sec_time * 1000 
+            seek_points(start_time, camera_id, args.api_key)
+
+    # Create Report
+    create_report_2var(temp_graph,hum_graph,data_type,temp_footage_anomalies,hum_footage_anomalies)
+
+    
+if __name__ == "__main__":
+    main()
